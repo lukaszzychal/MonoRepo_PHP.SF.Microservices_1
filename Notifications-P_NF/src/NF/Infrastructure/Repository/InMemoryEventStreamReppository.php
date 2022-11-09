e289fe754ec5afd906040144d3274bd4c3babe2e
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-11-09 12:31:02 +0100

    implemeent method EStream Reposotry

---
+++ Notifications-P_NF/src/NF/Infrastructure/Repository/InMemoryEventStreamReppository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\NF\Infrastructure\Repository;
+
+use App\NF\Infrastructure\Event\EventStream;
+use Exception;
+use Symfony\Component\Uid\Uuid;
+
+class InMemoryEventStreamReppository implements EventStreamRepositoryInterface
+{
+    /**
+     * @var array[
+     * 'string' => 'EventStream'  
+     * ]
+     *
+     */
+    private array $eventStream = [];
+
+    public function create(Uuid $uuid): void
+    {
+        return new EventStream($uuid, 0, null);
+    }
+
+    public function exist(Uuid $uuid): bool
+    {
+        return array_key_exists((string) $uuid, $this->eventStream);
+    }
+
+    public function get(Uuid $uuid): EventStream
+    {
+        if ($this->exist($uuid)) {
+            return $this->eventStream[$uuid];
+        }
+        /**
+         * @todo dodać konkreetny wyjątek
+         */
+        throw new Exception("No exist stream");
+    }
+}
