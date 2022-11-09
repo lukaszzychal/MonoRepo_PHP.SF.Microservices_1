a35bc4e54d63d9e1b5a7f40fbf81281b7e79b982
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-11-09 12:35:41 +0100

    add geter id

---
+++ Notifications-P_NF/src/NF/Infrastructure/Event/EventStream.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\NF\Infrastructure\Event;
+
+use DateTimeImmutable;
+use Symfony\Component\Uid\Uuid;
+
+class EventStream
+{
+    public function __construct(
+        private Uuid $id,
+        private int $version,
+        private DateTimeImmutable $updateAt = null,
+    ) {
+    }
+
+    /**
+     * Get the value of version
+     */
+    public function getVersion(): int
+    {
+        return $this->version;
+    }
+
+    public function IncremetVersion(): void
+    {
+        $this->version++;
+    }
+
+    public function updateDate(DateTimeImmutable $updateAt): void
+    {
+        $this->updateAt = $updateAt;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
