6e40a23a871260a5c4eb534f9e80bb19597ba8e8
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-11-13 15:36:31 +0100

    add new debugg propiertis

--- Notifications-P_NF/src/NF/Domain/Event/CreatedNotificationEvent.php
+++ Notifications-P_NF/src/NF/Domain/Event/CreatedNotificationEvent.php
@@ -13,16 +13,19 @@ class CreatedNotificationEvent implements DomainEventInterface
 {
     public \DateTimeImmutable $dateCalled;
     public string $eventName;
-
+    public string $placeOccurrence;
+    public string $placeCreated;
     public function __construct(
         public readonly NotificationId $id,
         public readonly TypeEnum $type,
         public readonly StatusEnum $status,
         public readonly DetailsNotification $details,
-        public readonly string $placeOccurrence
+        string $placeCreated = __METHOD__
     ) {
         $this->dateCalled = new \DateTimeImmutable();
         $this->eventName = (new \ReflectionClass($this))->getShortName();
+        $this->placeCreated = $placeCreated ?? __METHOD__;
+        $this->placeOccurrence = static::class;
     }
 
     public function getDateCalled(): \DateTimeImmutable
