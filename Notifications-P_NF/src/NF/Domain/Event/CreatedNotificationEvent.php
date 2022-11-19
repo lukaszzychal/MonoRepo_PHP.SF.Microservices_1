cbbfe917e62be21c1deaab571b465be1f738f288
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-11-19 23:00:43 +0100

    implement inmemeory agregate domain

--- Notifications-P_NF/src/NF/Domain/Event/CreatedNotificationEvent.php
+++ Notifications-P_NF/src/NF/Domain/Event/CreatedNotificationEvent.php
@@ -6,10 +6,12 @@ namespace App\NF\Domain\Event;
 
 use App\NF\Domain\Enum\StatusEnum;
 use App\NF\Domain\Enum\TypeEnum;
+use App\NF\Domain\Model\AggregateInterface;
 use App\NF\Domain\Model\DetailsNotification;
 use App\NF\Domain\Model\NotificationId;
+use Symfony\Component\Uid\Uuid;
 
-class CreatedNotificationEvent implements DomainEventInterface
+class CreatedNotificationEvent implements NotificationDomainEventInterface
 {
     public \DateTimeImmutable $dateCalled;
     public string $eventName;
@@ -21,6 +23,8 @@ class CreatedNotificationEvent implements DomainEventInterface
         public readonly TypeEnum $type,
         public readonly StatusEnum $status,
         public readonly DetailsNotification $details,
+        public readonly string $class,
+        public readonly AggregateInterface $notification,
         string $placeCreated = __METHOD__
     ) {
         $this->dateCalled = new \DateTimeImmutable();
@@ -32,5 +36,32 @@ class CreatedNotificationEvent implements DomainEventInterface
     public function getDateCalled(): \DateTimeImmutable
     {
         return $this->dateCalled;
+    }
+
+    public function getDetailsNotification(): DetailsNotification
+    {
+        return $this->details;
+    }
+
+    public function getId(): Uuid
+    {
+        return Uuid::fromString((string) $this->id);
+    }
+    public function getType(): TypeEnum
+    {
+        return $this->type;
+    }
+    public function getStatus(): StatusEnum
+    {
+        return $this->status;
+    }
+    public function getDetails(): DetailsNotification
+    {
+        return $this->details;
+    }
+
+    public function getObject(): AggregateInterface
+    {
+        return $this->notification;
     }
 }
