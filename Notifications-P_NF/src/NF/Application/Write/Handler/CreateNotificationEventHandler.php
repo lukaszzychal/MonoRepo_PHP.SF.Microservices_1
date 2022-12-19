16b1f511648ad84da4cb048429ff207fcdb276cd
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-12-19 22:09:26 +0100

    add test for handle

---
+++ Notifications-P_NF/src/NF/Application/Write/Handler/CreateNotificationEventHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\NF\Application\Write\Handler;
+
+use App\NF\Application\Write\Command\CreateEmailNotifcationCommand;
+use App\NF\Application\Write\Command\SendEmailCommand;
+use App\NF\Domain\Enum\TypeEnum;
+use App\NF\Domain\Model\EmailDetailsNotification;
+use App\NF\Domain\Model\Notification;
+use App\NF\Domain\Model\NotificationId;
+use App\NF\Domain\Repository\NotificationRepositoryInterface;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Messenger\Envelope as MessengerEnvelope;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DispatchAfterCurrentBusStamp;
+
+final class CreateNotificationEventHandler implements MessageHandlerInterface
+{
+
+    public function __construct(
+        private readonly NotificationRepositoryInterface $notificationRepository,
+        private readonly MessageBusInterface $commandBus
+    ) {
+    }
+    public function __invoke(CreateEmailNotifcationCommand $command): void
+    {
+
+        $notificationId = NotificationId::random();
+        $notification = Notification::create(
+            $notificationId,
+            TypeEnum::from($command->type),
+            new EmailDetailsNotification(
+                $command->email,
+                'email_from_config@test',
+                $command->subject,
+                $command->context
+            )
+        );
+
+        $this->notificationRepository->save($notification);
+
+        $this->commandBus->dispatch(
+            (new MessengerEnvelope(
+                new SendEmailCommand(
+                    $command->type,
+                    $command->email,
+                    $command->context,
+                    $command->subject
+                )
+            ))->with(new DispatchAfterCurrentBusStamp())
+        );
+    }
+}
---
+++ Notifications-P_NF/tests/Integration/NF/Application/CreateEmailNotifcationCommandTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\NF\Application;
+
+use App\NF\Application\Write\Command\CreateEmailNotifcationCommand;
+use App\NF\Application\Write\Handler\CreateNotificationEventHandler;
+use App\NF\Domain\Enum\TypeEnum;
+use App\NF\Domain\Model\Notification;
+use App\NF\Domain\Repository\NotificationRepositoryInterface;
+use App\NF\Infrastructure\Event\CreateNotificationEvent;
+use App\Tests\EmailNotificationTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\OrderBy;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+
+/**
+ * @group gg1
+ */
+class CreateEmailNotifcationCommandTest extends EmailNotificationTestCase
+{
+    public function testCreateEmailNotificationCommand(): void
+    {
+        $command = new CreateEmailNotifcationCommand(
+            TypeEnum::EMAIL->value,
+            'my.email@test',
+            'Hello :)',
+            'My subject'
+        );
+
+        $repo = $this->getContainer()->get(NotificationRepositoryInterface::class);
+        $cbus = $this->getContainer()->get(MessageBusInterface::class);
+
+        $handlerr = new CreateNotificationEventHandler(
+            $repo,
+            $cbus
+        );
+
+        $handlerr($command);
+
+        /**
+         * @var EntityManagerInterface $em
+         */
+        $em = $this->getContainer()->get(EntityManagerInterface::class);
+        $qb = $em->createQueryBuilder();
+        $qb->from(Notification::class, 'n');
+        $qb->select('n');
+        $result = $qb->getQuery()->getArrayResult();
+        $this->assertSame($command->subject, $result[count($result) - 1]['detailsNotification']['subject']);
+        $this->assertSame($command->context, $result[count($result) - 1]['detailsNotification']['body']);
+
+
+        $transport = $this->getTransport();
+        $this->assertCount(1, $transport->getSent());
+
+        $email = $this->getMailerMessage();
+        $this->assertEmailHtmlBodyContains($email, 'Hello :)');
+    }
+}
