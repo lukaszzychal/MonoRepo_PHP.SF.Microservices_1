37579a4e6c9d153cd0a5ecb1f742900af6771290
Lukasz Zychal <lukaszzpriv@gmail.com>
2022-12-19 18:06:03 +0100

    fix migrations

--- Notifications-P_NF/migrations/Version20221207191838.php
+++ Notifications-P_NF/migrations/Version20221207191838.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221121222229 extends AbstractMigration
+final class Version20221207191838 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,13 +19,14 @@ final class Version20221121222229 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA notification');
         $this->addSql('CREATE TABLE notification.event_stream (id UUID NOT NULL, event_number INT NOT NULL, update_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN notification.event_stream.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN notification.event_stream.update_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE notification.events (stream_id UUID NOT NULL, version INT NOT NULL, place_occurrence VARCHAR(255) NOT NULL, event_name VARCHAR(255) NOT NULL, event JSONB NOT NULL, PRIMARY KEY(stream_id))');
         $this->addSql('COMMENT ON COLUMN notification.events.stream_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE notification.notification (id UUID NOT NULL, status VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, details_notification JSONB NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN notification.notification.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
@@ -54,10 +55,10 @@ final class Version20221121222229 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE notification.event_stream');
         $this->addSql('DROP TABLE notification.events');
+        $this->addSql('DROP TABLE notification.notification');
         $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('DROP TABLE domain_event_failed');
     }
